84ce990c2ac88555a954ac08eb1b4757057a308a
Lwin Moe Paing <lwinmoepaing.uab@gmail.com>
2021-09-26 17:22:42 +0630

    Add: Template Upgradable Structure

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
+// Add Scss And Bootstrap ( yarn add -D  bootstrap@4.5.2 )
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./styles/index.scss";
+
 ReactDom.render(<App />, document.getElementById("electron-app"));
