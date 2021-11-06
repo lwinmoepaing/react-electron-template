bba4116d0de30add7445bd1f3b8407a84c883d64
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2021-11-06 19:44:20 +0630

    Change: Deploy

--- utils/preload.js
+++ utils/preload.js
@@ -1,5 +1,17 @@
 const { ipcRenderer, contextBridge } = require("electron");
 const path = require("path");
+const os = require("os");
+
+let __osAddress = [];
+for (let k in os.networkInterfaces()) {
+  for (let k2 in interfaces[k]) {
+    let address = interfaces[k][k2];
+    if (address.family === "IPv4" && !address.internal) {
+      __osAddress.push(address.address);
+    }
+  }
+}
+
 const { NOTI_CODE } = require("../config/constants");
 
 const __imageDir = path.join(__dirname, "../", "public", "assets", "images");
@@ -25,4 +37,5 @@ contextBridge.exposeInMainWorld("electron", {
   // Some Constants
   __dirname: __dirname,
   __imageDir: __imageDir,
+  __osAddress: __osAddress,
 });
