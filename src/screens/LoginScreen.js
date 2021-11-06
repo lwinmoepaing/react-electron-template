beb6679680a43f7a261a22b6fc5adb3685465ca8
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2021-11-07 03:47:17 +0630

    Update

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -8,8 +8,9 @@ import axios from "axios";
 
 export default function LoginScreen() {
   const [isLogin, setIsLogin] = useState(false);
+  const [getVersionLoading, setGetVersionLoading] = useState(false);
   const user = useSelector(({ profile }) => profile.data);
-  d;
+
   useEffect(() => {
     if (user) {
       setIsLogin(true);
@@ -30,6 +31,21 @@ export default function LoginScreen() {
     }
   };
 
+  const getVersion = async () => {
+    try {
+      setGetVersionLoading(true);
+      setTimeout(() => {
+        console.log("getVersion", electron);
+        setGetVersionLoading(false);
+      }, 1500);
+      // const res = await axios.get("http://localhost:5050/api");
+      console.log(res.data);
+    } catch (e) {
+      setGetVersionLoading(false);
+      console.log(e);
+    }
+  };
+
   return isLogin ? (
     <Redirect to={HOME_SCREEN.path} />
   ) : (
@@ -37,6 +53,7 @@ export default function LoginScreen() {
       <h2> Login Screen </h2>
       <LoginForm />
       <button onClick={sendRequest}>ON CLick Request</button>
+      <button onClick={getVersion}>get Version</button>
     </div>
   );
 }
