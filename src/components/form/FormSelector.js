e104d5b545d03d1f144f7027c8840e02323f74b6
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2021-11-20 01:56:20 +0630

    Update: Form Selector with Formik

--- src/components/form/FormSelector.js
+++ src/components/form/FormSelector.js
@@ -1,28 +1,49 @@
-import { ErrorMessage, Field } from "formik";
-import React from "react";
+import { ErrorMessage, useFormikContext } from "formik";
+import React, { useCallback } from "react";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 import FormErrorMessage from "./FormErrorMessage";
 
 export default function FormSelector(props) {
-  const { label, name, options, valueExtractor, labelExtractor, ...rest } =
-    props;
+  const { name, options, valueExtractor, labelExtractor, ...rest } = props;
 
-  const valueExtract = (item) =>
-    valueExtractor ? valueExtractor(item) : item.value;
+  const { values, setFieldValue, touched, errors } = useFormikContext();
 
-  const labelExtract = (item) =>
-    labelExtractor ? labelExtractor(item) : item.key;
+  const valueExtract = useCallback(
+    (item) => (valueExtractor ? valueExtractor(item) : item?.value || ""),
+    [options, valueExtractor]
+  );
+
+  const labelExtract = useCallback(
+    (item) => (labelExtractor ? labelExtractor(item) : item?.key || ""),
+    [options, labelExtractor]
+  );
+
+  const onHandleChange = (event) => {
+    setFieldValue(name, event.target.value);
+  };
 
   return (
-    <div className="form-control">
-      <label htmlFor={name}> {label} </label>
-      <Field name={name} id={name} as="select" {...rest}>
+    <FormControl fullWidth sx={{ my: 1 }}>
+      <TextField
+        name={name}
+        id={name}
+        select
+        size="small"
+        {...rest}
+        onChange={onHandleChange}
+        value={values[name]}
+      >
         {options.map((item) => (
-          <option key={valueExtract(item)} value={valueExtract(item)}>
+          <MenuItem key={labelExtract(item)} value={valueExtract(item)}>
             {labelExtract(item)}
-          </option>
+          </MenuItem>
         ))}
-      </Field>
-      <ErrorMessage name={name} component={FormErrorMessage} />
-    </div>
+      </TextField>
+      {touched[name] && errors[name] && (
+        <ErrorMessage name={name} render={FormErrorMessage} />
+      )}
+    </FormControl>
   );
 }
